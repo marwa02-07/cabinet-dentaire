7d794b3caf5ba75da8f4a97d098ef94a26607b40
marwa02-07 <benzianemarwa4@gmail.com>
2026-05-04 21:43:43 +0100

    final

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'cabinet_dentaire');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_PORT', 3307);
+define('DB_PORT', 3306);
 define('DB_CHARSET', 'utf8mb4');
 
 // Autres configurations
